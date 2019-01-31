95ab119cdc799f160d23ec9294ec54e8db11ab04
RolandHyden <Roland.Hyden@RegionHalland.se>
2019-01-31 15:36:10 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# region-halland-prepare-the-content
-Front-end-plugin som lägger till id med slug till alla rubriker (h2,h3 och h4)
+# Preparera "the_content" med id och slug
+
+## Hur man använder Region Hallands plugin "region-halland-prepare-the-content"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-prepare-the-content".
+
+
+## Användningsområde
+
+Denna plugin lägger till id och slug för alla h2, h3 och h4-rubriker.
+Detta kan vara användbart om man exempelvis vill bygga en "hitta-på-sidan".
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-prepare-the-content.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-prepare-the-content.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-prepare-the-content": "1.0.0"
+},
+```
+
+
+## Hur man använder "region-halland-prepare-the-content" på en sida via "Blade"
+
+```sh
+@if(function_exists('get_region_halland_prepare_the_content'))
+	@php(get_region_halland_prepare_the_content())
+@endif
+<article class="article">
+	{!! the_content() !!}
+</article>				
+```
+
+
+## Detta skapar då id med slug inuti "the_content" enligt:
+
+```sh
+<h2 id="lorem-ipsum">Lorem ipsum</h2>
+<h3 id="lorem-ipsum-dolares">Lorem ipsum dolares</h3>
+<h4 id="lorem-integer-metus">Lorem integer metus</h4>
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_prepare_the_content.php
@@ -0,0 +1,110 @@
+<?php
+
+	/**
+	 * @package Region Halland Prepare The Content
+	 */
+	/*
+	Plugin Name: Region Halland Prepare The Content
+	Description: Lägger till ett ID med slut i alla rubriker (h2, h3 och h4)
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Lägger till id med slut till alla rubriker och returnerar detta till wordpress
+	function get_region_halland_prepare_the_content() {
+		
+        // Wordpress-filter för att manipulera "the_content"
+		add_filter('the_content', function($content) {
+           return addIdToHeadings($content);
+        });
+
+	}
+
+	function addIdToHeadings($content) {
+
+        // Hämta aktuell post
+        global $post;
+
+        // Lägg innehållet i "the_content" i en variabel
+        $pageContent = $post->post_content;
+        
+        // Splitta upp the_content på "radbryt"
+        $content = "";
+        $arrPageContents = explode(PHP_EOL, $pageContent);
+        
+        // Loopa igenom arrayen och ersätt respektive rubrik med id och slug
+        foreach ($arrPageContents as $arrPageContent) {
+            $checkPageContent = substr($arrPageContent,1,2);
+            switch ($checkPageContent) {
+                case 'h2':
+                    $tmpContentH2 = str_replace("<h2>","",$arrPageContent);
+                    $tmpContentH2 = str_replace("</h2>","",$tmpContentH2);
+                    $toSlugH2     = prepare_for_the_content($tmpContentH2);
+                    $withIDH2     = str_replace("h2>","h2 id='".$toSlugH2."'>",$arrPageContent);
+                    $content      .= $withIDH2;
+                    break;
+                case 'h3':
+                    $tmpContentH3 = str_replace("<h3>","",$arrPageContent);
+                    $tmpContentH3 = str_replace("</h3>","",$tmpContentH3);
+                    $toSlugH3     = prepare_for_the_content($tmpContentH3);
+                    $withIDH3     = str_replace("h3>","h3 id='".$toSlugH3."'>",$arrPageContent);
+                    $content      .= $withIDH3;
+                    break;
+                case 'h4':
+                    $tmpContentH4 = str_replace("<h4>","",$arrPageContent);
+                    $tmpContentH4 = str_replace("</h4>","",$tmpContentH4);
+                    $toSlugH4     = prepare_for_the_content($tmpContentH4);
+                    $withIDH4     = str_replace("h4>","h4 id='".$toSlugH4."'>",$arrPageContent);
+                    $content      .= $withIDH4;
+                    break;
+                default:
+                    $content       .= "<p>" . $arrPageContent . "</p>";
+            }
+        }
+
+        // Returnera preparerad "the_content"
+        return $content;
+
+    }
+
+    // Funktion för att rensa bort eventuella specialtecken som inte får finnas i en slug
+    function prepare_for_the_content($content) {
+
+        $tmpSlug = str_replace(" ","-",$content);
+        $tmpSlug = str_replace("(","",$tmpSlug);
+        $tmpSlug = str_replace(")","",$tmpSlug);
+        $tmpSlug = str_replace("?","",$tmpSlug);
+        $tmpSlug = str_replace("!","",$tmpSlug);
+        $tmpSlug = str_replace("Ö","o",$tmpSlug);
+        $tmpSlug = str_replace("ö","o",$tmpSlug);
+        $tmpSlug = str_replace("Ä","a",$tmpSlug);
+        $tmpSlug = str_replace("ä","a",$tmpSlug);
+        $tmpSlug = str_replace("Å","a",$tmpSlug);
+        $tmpSlug = str_replace("å","a",$tmpSlug);
+        $tmpSlug = trim($tmpSlug);
+        $tmpSlug = strtolower($tmpSlug);
+
+        // Returnera rensad slug
+        return $tmpSlug;
+                    
+    }
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_prepare_the_content_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_prepare_the_content_deactivate() {
+        // Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+    register_activation_hook( __FILE__, 'region_halland_prepare_the_content_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+    register_deactivation_hook( __FILE__, 'region_halland_prepare_the_content_deactivate');
+
+?>
